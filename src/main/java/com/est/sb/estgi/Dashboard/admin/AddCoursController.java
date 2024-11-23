a7794f50c06eb6d2c13dcea7206e01834a80be07
Ysn <dev1977belmousse@gmail.com>
2024-11-23 07:39:43 +0100

    working on it

--- src/main/java/com/est/sb/estgi/Dashboard/admin/AddCoursController.java
+++ src/main/java/com/est/sb/estgi/Dashboard/admin/AddCoursController.java
@@ -1,12 +1,45 @@
 package com.est.sb.estgi.Dashboard.admin;
 
 import com.est.sb.estgi.DashboardController;
+import com.est.sb.estgi.DatabaseHelper;
+import com.est.sb.estgi.actors.Cours;
+import com.est.sb.estgi.actors.Role;
+import com.est.sb.estgi.actors.Teacher;
+import com.est.sb.estgi.actors.User;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.ComboBox;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddCoursController {
 
+    @FXML
+    private ComboBox<String> teacherList;
+    private List<Teacher> teachers = new ArrayList<>();
+    @FXML
+    public void initialize() {
+        try{
+            for (User teacher: DatabaseHelper.getAll(Role.TEACHER)){
+                teachers.add((Teacher) teacher);
+                teacherList.getItems().add(teacher.getName());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            teacherList.getItems().addAll("Admin", "Teacher");
+        }
+    }
+
+    @FXML
+    public void handleSubmitButton(){
+
+        System.out.println(teacherList.getSelectionModel().getSelectedItem());
+        System.out.println(teachers.get(teacherList.getSelectionModel().getSelectedIndex()).getName());
+
+
+    }
     @FXML
     public void handleBackButton(){
         try{
